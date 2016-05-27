c8e8d4ddfe52b74c8468d97345fb68df89dfc5f6
magic-lantern <magic-lantern@users.noreply.github.com>
2016-05-27 11:01:08 -0600

    Created initial non-interactive HTML mockup

--- safor/app/router.js
+++ safor/app/router.js
@@ -8,6 +8,7 @@ const Router = Ember.Router.extend({
 Router.map(function() {
   this.route('viewannotation');
   this.route('editannotation');
+  this.route('diseaseconcepts');
 });
 
 export default Router;
---
+++ safor/tests/unit/routes/diseaseconcepts-test.js
@@ -0,0 +1,11 @@
+import { moduleFor, test } from 'ember-qunit';
+
+moduleFor('route:diseaseconcepts', 'Unit | Route | diseaseconcepts', {
+  // Specify the other units that are required for this test.
+  // needs: ['controller:foo']
+});
+
+test('it exists', function(assert) {
+  let route = this.subject();
+  assert.ok(route);
+});
